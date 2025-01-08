bcfcf59adb9418ee2f21bb8ece298baa0e6d887c
Eric Paul <ericpaul105@gmail.com>
2025-01-08 16:00:02 -0600

    Added all TODO tests for add function requirements

---
+++ test/main.test.ts
@@ -0,0 +1,12 @@
+describe("add", () => {
+  test.todo("should return 0 for an empty string");
+  test.todo("should return the number itself for a single number string");
+  test.todo(
+    "should return the sum of two numbers for a string with two numbers"
+  );
+  test.todo("should handle any number of numbers");
+  test.todo("should handle new lines as delimiters");
+  test.todo("should support custom delimiters");
+  test.todo("should throw an exception for negative numbers");
+  test.todo("should include all negative numbers in the exception message");
+});
